10ca8804c0f64f67407de178017fbf3e79ea0dba
cookie99999 <77994115+cookie99999@users.noreply.github.com>
2023-02-02 22:39:24 -0500

    Switch to extended mode int13/ah42 loading

--- disk.s
+++ disk.s
@@ -1,40 +1,37 @@
 disk_load:
+	;; dl -> disk to load
+	;; ds:si -> ptr to disk address packet
+
+	;; disk address packet:
+	;; byte: packet size
+	;; byte: always 0
+	;; word: num sectors
+	;; dword: buf pointer (seg:offs little endian)
+	;; dword: lower dword of start lba
+	;; dword: upper word of start lba
+
 	pusha
-	push dx
 
 	mov al, dl
 	mov ah, 0x0e
 	mov bh, 0x00
-	int 0x10
-	
-	mov ah, 0x02		;read mode
-	mov al, dh		;num sectors
-	mov cl, 0x02		;start sector (sec 1 is mbr)
-	mov ch, 0x00		;cyl
-	mov dh, 0x00		;head
+	int 0x10 		;todo: hex print function
 
+	mov ah, 0x42
 	int 0x13
 	jc disk_error
-
-	pop dx
-	cmp al, dh
-	jne sectors_error
 
 	popa
 	ret
 
 disk_error:
 	mov al, ah
-	jmp loop
-
-sectors_error:
-	mov al, 'S'
-	jmp loop
-
-loop:
 	mov ah, 0x0e
 	mov bh, 0x00
 	int 0x10
+
+	mov word cx, [ds:si + 2]
+
 	mov al, 'X'
 	int 0x10
 	jmp $
