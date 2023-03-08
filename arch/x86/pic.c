c8bb255c29839759a59d91ff910e3dfbd1d97c57
cookie99999 <77994115+cookie99999@users.noreply.github.com>
2023-03-08 16:57:00 -0500

    Add PIC code

---
+++ arch/x86/pic.c
@@ -0,0 +1,62 @@
+#include <stdint.h>
+
+#include "pic.h"
+
+void remap_pics(uint8_t primary_offs, uint8_t secondary_offs) {
+  uint8_t m1, m2; //mask backup
+  m1 = inb(PIC_PRIMARY_DATA);
+  m2 = inb(PIC_SECONDARY_DATA);
+
+  outb(PIC_PRIMARY_CMD, PIC_ICW1_INIT | PIC_ICW1_ICW4);
+  outb(PIC_SECONDARY_CMD, PIC_ICW1_INIT | PIC_ICW1_ICW4);
+  outb(PIC_PRIMARY_DATA, primary_offs);
+  outb(PIC_SECONDARY_DATA, secondary_offs);
+  outb(PIC_PRIMARY_DATA, 0x04); //secondary pic present
+  outb(PIC_SECONDARY_DATA, 0x02); //cascade id
+  outb(PIC_PRIMARY_DATA, PIC_ICW4_8086);
+  outb(PIC_SECONDARY_DATA, PIC_ICW4_8086);
+
+  //restore masks
+  outb(PIC_PRIMARY_DATA, m1);
+  outb(PIC_SECONDARY_DATA, m2);
+}
+
+void disable_pics() {
+  outb(PIC_PRIMARY_DATA, 0xff);
+  outb(PIC_SECONDARY_DATA, 0xff);
+}
+
+void send_eoi(int num) {
+  if (num >= 0x28)
+    outb(PIC_SECONDARY_CMD, PIC_EOI);
+  outb(PIC_PRIMARY_CMD, PIC_EOI);
+}
+
+void pic_set_mask(uint8_t irq) {
+  uint16_t port;
+  uint8_t mask;
+
+  if (irq < 8) {
+    port = PIC_PRIMARY_DATA;
+  } else {
+    port = PIC_SECONDARY_DATA;
+    irq -= 8;
+  }
+  mask = inb(port) | (1 << irq);
+  outb(port, mask);
+}
+
+void pic_clear_mask(uint8_t irq) {
+  uint16_t port;
+  uint8_t mask;
+
+  if (irq < 8) {
+    port = PIC_PRIMARY_DATA;
+  } else {
+    port = PIC_SECONDARY_DATA;
+    irq -= 8;
+  }
+
+  mask = inb(port) & ~(1 << irq);
+  outb(port, mask);
+}
---
+++ arch/x86/pic.h
@@ -0,0 +1,25 @@
+#ifndef PIC_H
+#define PIC_H
+
+#define PIC_PRIMARY_CMD 0x20
+#define PIC_PRIMARY_DATA 0x21
+#define PIC_SECONDARY_CMD 0xa0
+#define PIC_SECONDARY_DATA 0xa1
+
+#define PIC_EOI 0x20 //end of interrupt
+
+#define PIC_ICW1_ICW4 0x01
+#define PIC_ICW1_SINGLE 0x02
+#define PIC_ICW1_ADDR_INTERVAL 0x04
+#define PIC_ICW1_LEVEL_TRIG 0x08
+#define PIC_ICW1_INIT 0x10 //required to be set
+
+#define PIC_ICW4_8086 0x01
+#define PIC_ICW4_AUTO_EOI 0x02
+#define PIC_ICW4_BUF_SECONDARY 0x08
+#define PIC_ICW4_BUF_PRIMARY 0x0c
+#define PIC_ICW4_FULLY_NESTED 0x10
+
+void remap_pics(uint8_t primary_offs, uint8_t secondary_offs);
+void disable_pics(void);
+void send_eoi(int n);
